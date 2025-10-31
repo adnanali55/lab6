22fec2bcbd607fa6220dbde89fbc9accb2305208
adnanali55 <f2025-0968@bnu.edu.pk>
2025-10-31 14:40:35 +0500

    Implement gas selection and distance calculation

---
+++ task6(switch).cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int choice;
+    double time, speed, distance;
+    cout << "Select a gas from the menu below:\n";
+    cout << "1. Carbon Dioxide\n";
+    cout << "2. Air\n";
+    cout << "3. Helium\n";
+    cout << "4. Hydrogen\n";
+    cout << "Enter the number corresponding to your choice: ";
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            speed = 258.0;
+            cout << "You selected Carbon Dioxide.\n";
+            break;
+        case 2:
+            speed = 331.5;
+            cout << "You selected Air.\n";
+            break;
+        case 3:
+            speed = 972.0;
+            cout << "You selected Helium.\n";
+            break;
+        case 4:
+            speed = 1270.0;
+            cout << "You selected Hydrogen.\n";
+            break;
+        default:
+            cout << "Invalid menu choice. Program ending.\n";
+            return 0;
+    }
+
+    cout << "Enter the time (in seconds): ";
+    cin >> time;
+    if (time < 0 || time > 30) {
+        cout << "Invalid time. Time must be between 0 and 30 seconds.\n";
+        return 0;
+    }
+
+    distance = speed * time;
+
+    cout << fixed << setprecision(2);
+    cout << "\nSpeed of sound: " << speed << " m/s\n";
+    cout << "Time: " << time << " seconds\n";
+    cout << "Distance: " << distance << " meters\n";
+
+    return 0;
+}
